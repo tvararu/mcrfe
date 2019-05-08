a3c949227d5a6788a4f268582d6ed66e9e6ce3b7
Theodor Vararu <theo@vararu.org>
2019-05-08 20:41:38 +0100

    Remove demo code

--- components/PostList.js
+++ components/PostList.js
@@ -1,7 +1,6 @@
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import ErrorMessage from "./ErrorMessage";
-import PostUpvoter from "./PostUpvoter";
 
 export const allPostsQuery = gql`
   query allPosts($first: Int!, $skip: Int!) {
@@ -38,77 +37,13 @@ export default function PostList() {
                   <div>
                     <span>{index + 1}. </span>
                     <a href={post.url}>{post.title}</a>
-                    <PostUpvoter id={post.id} votes={post.votes} />
                   </div>
                 </li>
               ))}
             </ul>
-            {areMorePosts ? (
-              <button onClick={() => loadMorePosts(allPosts, fetchMore)}>
-                {" "}
-                {loading ? "Loading..." : "Show More"}{" "}
-              </button>
-            ) : (
-              ""
-            )}
-            <style jsx>{`
-              section {
-                padding-bottom: 20px;
-              }
-              li {
-                display: block;
-                margin-bottom: 10px;
-              }
-              div {
-                align-items: center;
-                display: flex;
-              }
-              a {
-                font-size: 14px;
-                margin-right: 10px;
-                text-decoration: none;
-                padding-bottom: 0;
-                border: 0;
-              }
-              span {
-                font-size: 14px;
-                margin-right: 5px;
-              }
-              ul {
-                margin: 0;
-                padding: 0;
-              }
-              button:before {
-                align-self: center;
-                border-style: solid;
-                border-width: 6px 4px 0 4px;
-                border-color: #ffffff transparent transparent transparent;
-                content: "";
-                height: 0;
-                margin-right: 5px;
-                width: 0;
-              }
-            `}</style>
           </section>
         );
       }}
     </Query>
   );
-}
-
-function loadMorePosts(allPosts, fetchMore) {
-  fetchMore({
-    variables: {
-      skip: allPosts.length
-    },
-    updateQuery: (previousResult, { fetchMoreResult }) => {
-      if (!fetchMoreResult) {
-        return previousResult;
-      }
-      return Object.assign({}, previousResult, {
-        // Append the new posts results to the old one
-        allPosts: [...previousResult.allPosts, ...fetchMoreResult.allPosts]
-      });
-    }
-  });
 }
