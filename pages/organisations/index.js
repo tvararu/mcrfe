d20af79b46a2f42605501c72041912aea983ed72
Theodor Vararu <theo@vararu.org>
2019-05-12 18:04:21 +0100

    Refactor organisations show page

--- pages/organisations/index.js
+++ pages/organisations/index.js
@@ -14,6 +14,92 @@ const allProvidersQuery = gql`
   }
 `;
 
+const Content = ({ providerCode }) => (
+  <div className="govuk-grid-row">
+    <div className="govuk-grid-column-two-thirds">
+      <h2 className="govuk-heading-m">
+        <a
+          className="govuk-link"
+          href={`/organisations/details?providerCode=${providerCode}`}
+        >
+          About your organisation
+        </a>
+      </h2>
+      <p className="govuk-body">Use this section to:</p>
+      <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-8">
+        <li>write about your organisation</li>
+        <li>set your contact details</li>
+        <li>publish this information on all course pages</li>
+      </ul>
+
+      <h2 className="govuk-heading-m">
+        <a
+          className="govuk-link"
+          href={`/organisations/locations?providerCode=${providerCode}`}
+        >
+          Locations
+        </a>
+      </h2>
+      <p className="govuk-body">Use this section to:</p>
+      <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-8">
+        <li>edit location names and addresses</li>
+        <li>add locations</li>
+      </ul>
+
+      <h2 className="govuk-heading-m">
+        <Link
+          prefetch
+          as={`/organisations/${providerCode}/courses`}
+          href={`/organisations/courses?providerCode=${providerCode}`}
+        >
+          <a className="govuk-link">Courses</a>
+        </Link>
+      </h2>
+      <p className="govuk-body">Use this section to:</p>
+      <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-8">
+        <li>write about each course</li>
+        <li>preview and publish courses</li>
+        <li>copy content between courses</li>
+      </ul>
+
+      <h2 className="govuk-heading-m">
+        <a
+          className="govuk-link"
+          href={`/organisations/request-access?providerCode=${providerCode}`}
+        >
+          Request access for someone else
+        </a>
+      </h2>
+      <p className="govuk-body govuk-!-margin-bottom-8">
+        You can request a DfE Sign-in account for others who manage your
+        courses.
+      </p>
+    </div>
+    <div className="govuk-grid-column-one-third">
+      <style jsx>{`
+        .related {
+          border-top: 5px solid #005ea5;
+          background-color: #f8f8f8;
+          padding: 20px;
+        }
+      `}</style>
+      <div className="related">
+        <h2 className="govuk-heading-m">Support and guidance</h2>
+        <p className="govuk-body govuk-!-margin-bottom-0">
+          If you have a question, or you’ve had a problem using Publish, you can{" "}
+          <a
+            className="govuk-link"
+            href="mailto:becomingateacher@digital.education.gov.uk?subject=Support%20and%20guidance"
+          >
+            email the Becoming a Teacher team
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
 const OrganisationsPage = ({ providerCode }) => (
   <Query query={allProvidersQuery} variables={{ providerCode }}>
     {({ loading, error, data: { allProviders } }) => {
@@ -31,89 +117,7 @@ const OrganisationsPage = ({ providerCode }) => (
           />
           <GovukMain>
             <h1 className="govuk-heading-xl">{providerName}</h1>
-            <div className="govuk-grid-row">
-              <div className="govuk-grid-column-two-thirds">
-                <h2 className="govuk-heading-m">
-                  <a
-                    className="govuk-link"
-                    href={`/organisations/details?providerCode=${providerCode}`}
-                  >
-                    About your organisation
-                  </a>
-                </h2>
-                <p className="govuk-body">Use this section to:</p>
-                <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-8">
-                  <li>write about your organisation</li>
-                  <li>set your contact details</li>
-                  <li>publish this information on all course pages</li>
-                </ul>
-
-                <h2 className="govuk-heading-m">
-                  <a
-                    className="govuk-link"
-                    href={`/organisations/locations?providerCode=${providerCode}`}
-                  >
-                    Locations
-                  </a>
-                </h2>
-                <p className="govuk-body">Use this section to:</p>
-                <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-8">
-                  <li>edit location names and addresses</li>
-                  <li>add locations</li>
-                </ul>
-
-                <h2 className="govuk-heading-m">
-                  <Link
-                    as={`/organisations/${providerCode}/courses`}
-                    href={`/organisations/courses?providerCode=${providerCode}`}
-                  >
-                    <a className="govuk-link">Courses</a>
-                  </Link>
-                </h2>
-                <p className="govuk-body">Use this section to:</p>
-                <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-8">
-                  <li>write about each course</li>
-                  <li>preview and publish courses</li>
-                  <li>copy content between courses</li>
-                </ul>
-
-                <h2 className="govuk-heading-m">
-                  <a
-                    className="govuk-link"
-                    href={`/organisations/request-access?providerCode=${providerCode}`}
-                  >
-                    Request access for someone else
-                  </a>
-                </h2>
-                <p className="govuk-body govuk-!-margin-bottom-8">
-                  You can request a DfE Sign-in account for others who manage
-                  your courses.
-                </p>
-              </div>
-              <div className="govuk-grid-column-one-third">
-                <style jsx>{`
-                  .related {
-                    border-top: 5px solid #005ea5;
-                    background-color: #f8f8f8;
-                    padding: 20px;
-                  }
-                `}</style>
-                <div className="related">
-                  <h2 className="govuk-heading-m">Support and guidance</h2>
-                  <p className="govuk-body govuk-!-margin-bottom-0">
-                    If you have a question, or you’ve had a problem using
-                    Publish, you can{" "}
-                    <a
-                      className="govuk-link"
-                      href="mailto:becomingateacher@digital.education.gov.uk?subject=Support%20and%20guidance"
-                    >
-                      email the Becoming a Teacher team
-                    </a>
-                    .
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Content providerCode={providerCode} />
           </GovukMain>
         </>
       );
