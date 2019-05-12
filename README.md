501895f9d6ff0ac3407a630c7ade10789879e257
Theodor Vararu <theo@vararu.org>
2019-05-12 11:47:32 +0100

    Add bundle analyzer plugin

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # mcrfe
 
+Run in development:
+
 ```bash
 yarn db manage
 yarn dev
 open http://localhost:3000
+```
+
+Analyze the client-side JS bundle:
+
+```bash
+BUNDLE_ANALYZE=client yarn build
 ```
 
 ## License
--- next.config.js
+++ next.config.js
@@ -1,18 +1,33 @@
 const path = require("path");
 const withSass = require("@zeit/next-sass");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = withSass({
-  webpack: config => {
-    config.plugins.push(
-      new CopyWebpackPlugin([
-        {
-          from: path.join(__dirname, "/node_modules/govuk-frontend/assets"),
-          to: path.join(__dirname, "/static/govuk-frontend/")
-        }
-      ])
-    );
+module.exports = withBundleAnalyzer(
+  withSass({
+    webpack: config => {
+      config.plugins.push(
+        new CopyWebpackPlugin([
+          {
+            from: path.join(__dirname, "/node_modules/govuk-frontend/assets"),
+            to: path.join(__dirname, "/static/govuk-frontend/")
+          }
+        ])
+      );
 
-    return config;
-  }
-});
+      return config;
+    },
+    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: "static",
+        reportFilename: "../bundles/server.html"
+      },
+      browser: {
+        analyzerMode: "static",
+        reportFilename: "../bundles/client.html"
+      }
+    }
+  })
+);
