37f1b95ee1d94e9747d5fc8e7dd46c353f142ba2
Theodor Vararu <theo@vararu.org>
2019-05-18 22:45:38 +0100

    Remove webpack-copy-plugin, add instructions to vendor govuk-frontend

--- README.md
+++ README.md
@@ -21,6 +21,13 @@ Analyze the client-side JS bundle:
 BUNDLE_ANALYZE=client yarn build
 ```
 
+Vendor `govuk-frontend` assets (re-run when the version updates):
+
+```bash
+cp -nrv node_modules/govuk-frontend/assets/ static/govuk-frontend/
+git add static && git commit -m "Update vendored govuk-frontend assets"
+```
+
 ## License
 
 [MIT](LICENSE.txt).
--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
-const path = require("path");
 const withSass = require("@zeit/next-sass");
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = withBundleAnalyzer(
   withSass({
-    webpack: config => {
-      config.plugins.push(
-        new CopyWebpackPlugin([
-          {
-            from: path.join(__dirname, "/node_modules/govuk-frontend/assets"),
-            to: path.join(__dirname, "/static/govuk-frontend/")
-          }
-        ])
-      );
-
-      return config;
-    },
     analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
     analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
     bundleAnalyzerConfig: {
