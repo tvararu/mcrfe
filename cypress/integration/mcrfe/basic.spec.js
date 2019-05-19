77b8f6b3a646a232145eda6acad00857c722de0e
Theodor Vararu <theo@vararu.org>
2019-05-19 22:48:55 +0100

    Refactor cy.visit and add cy.assertReloadAssert

--- cypress/integration/mcrfe/basic.spec.js
+++ cypress/integration/mcrfe/basic.spec.js
@@ -1,72 +1,54 @@
-const auth = {
-  username: "local",
-  password: "local"
-};
-
 const schoolName = "2Schools Consortium";
 
 describe("Basic tests", () => {
   describe("Fresh visit", () => {
     it("Organisations page", () => {
-      cy.visit("/", { auth });
+      cy.visit("/");
 
-      cy.contains("Organisations").should("exist");
-      cy.contains(schoolName).should("exist");
-
-      cy.reload();
-
-      cy.contains("Organisations").should("exist");
-      cy.contains(schoolName).should("exist");
+      cy.assertReloadAssert(() => {
+        cy.contains("Organisations").should("exist");
+        cy.contains(schoolName).should("exist");
+      });
     });
 
     it("Organisation page", () => {
-      cy.visit("/organisations/T92", { auth });
+      cy.visit("/organisations/T92");
 
-      cy.contains(schoolName).should("exist");
-      cy.contains("Courses").should("exist");
-
-      cy.reload();
-
-      cy.contains(schoolName).should("exist");
-      cy.contains("Courses").should("exist");
+      cy.assertReloadAssert(() => {
+        cy.contains(schoolName).should("exist");
+        cy.contains("Courses").should("exist");
+      });
     });
 
     it("Courses page", () => {
-      cy.visit("/organisations/T92/courses", { auth });
+      cy.visit("/organisations/T92/courses");
 
-      cy.contains("Courses").should("exist");
-      cy.contains("Primary (X130)").should("exist");
-
-      cy.reload();
-
-      cy.contains("Courses").should("exist");
-      cy.contains("Primary (X130)").should("exist");
+      cy.assertReloadAssert(() => {
+        cy.contains("Courses").should("exist");
+        cy.contains("Primary (X130)").should("exist");
+      });
     });
   });
 
   describe("Navigation between pages", () => {
     it("Organisations to Organisation", () => {
-      cy.visit("/", { auth });
+      cy.visit("/");
 
       cy.contains(schoolName).click();
 
-      cy.get("h1").should("contain", schoolName);
-
-      cy.reload();
-
-      cy.get("h1").should("contain", schoolName);
+      cy.assertReloadAssert(() => {
+        cy.get("h1").should("contain", schoolName);
+      });
     });
 
     it("Organisation to Courses", () => {
-      cy.visit("/organisations/T92", { auth });
+      cy.visit("/organisations/T92");
 
       cy.contains("Courses").click();
 
-      cy.get("h1").should("contain", "Courses");
-
-      cy.reload();
-
-      cy.get("h1").should("contain", "Courses");
+      cy.assertReloadAssert(() => {
+        cy.get("h1").should("contain", "Courses");
+      });
     });
   });
 });
