5bed14d165cbe8641d1afc0904b0f04a61fb21d7
Theodor Vararu <theo@vararu.org>
2019-05-21 15:25:02 +0100

    Render using govuk-frontend by default
    Allow users to upgrade to React using a cookie.

--- pages/_document.js
+++ pages/_document.js
@@ -10,21 +10,45 @@ const accessDenied = res => {
   return {};
 };
 
+const Script = ({ children }) => (
+  <script dangerouslySetInnerHTML={{ __html: `(${children.toString()})();` }} />
+);
+
+const addJsEnabled = () => {
+  document.body.classList.add("js-enabled");
+};
+
+const govukInit = () => {
+  GOVUKFrontend.initAll();
+};
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     if (!authValid(ctx.req.headers)) return accessDenied(ctx.res);
 
+    const spaMode = ctx.req.headers["cookie"].includes("spaMode=true");
+
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return { ...initialProps, spaMode };
   }
 
   render() {
+    const { spaMode } = this.props;
     return (
       <html className="govuk-template">
         <Head />
         <body className="govuk-template__body">
+          <Script>{addJsEnabled}</Script>
           <Main />
-          <NextScript />
+
+          {spaMode ? (
+            <NextScript />
+          ) : (
+            <>
+              <script src="/static/govuk-frontend/scripts/all.js" />
+              <Script>{govukInit}</Script>
+            </>
+          )}
         </body>
       </html>
     );
