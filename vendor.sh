5e48650aed9708a6e0c8d0acd64c90fc244ef780
Theodor Vararu <theo@vararu.org>
2019-05-22 20:49:52 +0100

    Move favicon to /static

--- vendor.sh
+++ vendor.sh
@@ -2,5 +2,6 @@
 set -e
 
 cp -rv node_modules/govuk-frontend/assets/ static/govuk-frontend/
+mv static/govuk-frontend/images/favicon.ico static/
 cp -v node_modules/govuk-frontend/all.js static/govuk-frontend/scripts
 yarn run uglifyjs --compress --mangle -o static/govuk-frontend/scripts/all.min.js static/govuk-frontend/scripts/all.js
