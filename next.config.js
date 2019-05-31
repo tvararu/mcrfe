44df190e6db0a1fc2361f22ac5df244e9e16f80d
Theodor Vararu <theo@vararu.org>
2019-05-31 10:41:42 +0100

    Add next-offline

--- next.config.js
+++ next.config.js
@@ -2,23 +2,26 @@ const withSass = require("@zeit/next-sass");
 const withSize = require("next-size");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
+const withOffline = require("next-offline");
 
-module.exports = withSize(
-  withSass({
-    webpack: config => {
-      if (process.env.BUNDLE_ANALYZE) {
-        config.plugins.push(
-          new BundleAnalyzerPlugin({
-            analyzerMode: "static"
-          })
-        );
-      }
-      return config;
-    },
-    env: {
-      AUTH: process.env.AUTH,
-      API_URL: process.env.API_URL
-    },
-    target: "serverless"
-  })
+module.exports = withOffline(
+  withSize(
+    withSass({
+      webpack: config => {
+        if (process.env.BUNDLE_ANALYZE) {
+          config.plugins.push(
+            new BundleAnalyzerPlugin({
+              analyzerMode: "static"
+            })
+          );
+        }
+        return config;
+      },
+      env: {
+        AUTH: process.env.AUTH,
+        API_URL: process.env.API_URL
+      },
+      target: "serverless"
+    })
+  )
 );
